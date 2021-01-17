5fbc2336a856d3484d922146d930030e46a3f6fb
Vince Sangkhae <vsangk@gmail.com>
2021-01-17 11:51:12 -0800

    [01] ec3 - setState with object or fn

--- src/exercise/01.js
+++ src/exercise/01.js
@@ -3,17 +3,24 @@
 
 import * as React from 'react'
 
-const countReducer = (state, action) => ({
-  ...state,
-  ...action,
-})
+const countReducer = (state, action) => {
+  if (typeof action === 'function') {
+    return action(state)
+  }
+
+  return {
+    ...state,
+    ...action,
+  }
+}
 
 function Counter({initialCount = 0, step = 1}) {
   const [state, setState] = React.useReducer(countReducer, {
     count: initialCount,
   })
   const {count} = state
-  const increment = () => setState({count: count + step})
+  const increment = () =>
+    setState(currentState => ({count: currentState.count + step}))
 
   return <button onClick={increment}>{count}</button>
 }
