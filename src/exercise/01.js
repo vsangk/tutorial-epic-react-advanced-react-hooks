498527da7fe135334874a4ced6e709ee6127e43f
Vince Sangkhae <vsangk@gmail.com>
2021-01-17 11:46:57 -0800

    [01] ec1 - setState with action

--- src/exercise/01.js
+++ src/exercise/01.js
@@ -3,14 +3,14 @@
 
 import * as React from 'react'
 
-const countReducer = (state, newState) => {
-  return newState
+const countReducer = (state, step) => {
+  return state + step
 }
 
 function Counter({initialCount = 0, step = 1}) {
-  const [count, setCount] = React.useReducer(countReducer, initialCount)
+  const [count, changeCount] = React.useReducer(countReducer, initialCount)
+  const increment = () => changeCount(step)
 
-  const increment = () => setCount(count + step)
   return <button onClick={increment}>{count}</button>
 }
 
